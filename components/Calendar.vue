b2fa9d16f876e9473dc148e384f5461da5a5759a
jamesandrews23 <james.andrews23@gmail.com>
2021-06-24 13:57:43 -0500

    Replaces Overview.vue with Calendar.vue for better naming. Formats date displayed in MM/DD/YYYY to match activity cards. Cleans up default.vue.

---
+++ components/Calendar.vue
@@ -0,0 +1,58 @@
+<template>
+	<v-menu
+		ref="dateMenu"
+		v-model="dateMenu"
+		:close-on-content-click="false"
+		transition="scale-transition"
+		offset-y
+		max-width="290px"
+		min-width="auto"
+	>
+		<template v-slot:activator="{ on, attrs }">
+			<v-text-field
+				v-model="dateFormatted"
+				prepend-icon="mdi-calendar"
+				v-bind="attrs"
+				@blur="date = parseDate(dateFormatted)"
+				v-on="on"
+				hide-details
+				single-line
+			></v-text-field>
+		</template>
+		<v-date-picker
+			v-model="date"
+			@input="dateMenu = false"
+		></v-date-picker>
+	</v-menu>
+</template>
+<script>
+    export default {
+        data: vm => ({
+            date: new Date().toISOString().substr(0, 10),
+            dateFormatted: vm.formatDate(new Date().toISOString().substr(0, 10)),
+            dateMenu: false,
+            menu2: false,
+        }),
+
+        watch: {
+            date (val) {
+                this.dateFormatted = this.formatDate(this.date)
+            },
+        },
+
+        methods: {
+            formatDate (date) {
+                if (!date) return null;
+
+                const [year, month, day] = date.split('-');
+                return `${month}/${day}/${year}`;
+            },
+            parseDate (date) {
+                if (!date) return null;
+
+                const [month, day, year] = date.split('/');
+                return `${year}-${month.padStart(2, '0')}-${day.padStart(2, '0')}`;
+            },
+        },
+    }
+</script>

c17cae254fa77366e50673f682e6b185336f01bb
jamesandrews23 <james.andrews23@gmail.com>
2021-07-01 17:05:42 -0500

    Adding base for gh-pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,9 @@ import colors from 'vuetify/es5/util/colors'
 export default {
 	mode: 'spa',
 	target: 'static',
+	router: {
+		base: '/fitty-mcfitpants-fit-tracker/'
+	},
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - fitty-mcfitpants',

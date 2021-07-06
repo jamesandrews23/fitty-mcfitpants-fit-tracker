a8afb2ceceff4acc1bb0804ef5d695bfba124f54
James <james.andrews23@gmail.com>
2021-07-06 14:53:38 -0500

    Fixes the header which allows the body of the card to take up the full width.

--- components/Activity.vue
+++ components/Activity.vue
@@ -2,39 +2,16 @@
   <v-card
     class="mt-3 mb-3"
   >
+		<v-card-title>
+			{{ type }}
+			<v-spacer />
+			<v-icon large :color="color">
+				{{ icon }}
+			</v-icon>
+		</v-card-title>
 		<v-card-text>
-			<v-container>
-				<v-row>
-					<v-col>
-						{{ type }}
-					</v-col>
-					<v-col>
-						<v-icon large :color="color">
-							{{ icon }}
-						</v-icon>
-					</v-col>
-				</v-row>
-				<v-row>
-					<component :is="content" ref="dynamicComponent"></component>
-				</v-row>
-			</v-container>
+			<component :is="content" ref="dynamicComponent"></component>
 		</v-card-text>
-<!--		<v-list-item three-line>-->
-<!--			<v-list-item-content>-->
-<!--				<v-list-item-title class="text-h5 mb-1">-->
-<!--					{{type}}-->
-<!--				</v-list-item-title>-->
-<!--				<v-list-item-subtitle>{{desc}}</v-list-item-subtitle>-->
-<!--				<component :is="content" ref="dynamicComponent"></component>-->
-<!--			</v-list-item-content>-->
-<!--			<v-list-item-avatar-->
-<!--				size="80"-->
-<!--			>-->
-<!--				<v-icon large :color="color">-->
-<!--					{{icon}}-->
-<!--				</v-icon>-->
-<!--			</v-list-item-avatar>-->
-<!--		</v-list-item>-->
     <v-card-actions>
       <v-btn
         text
